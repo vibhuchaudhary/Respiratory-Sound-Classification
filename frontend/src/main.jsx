a25c585c32aea350e8b3414d9e7e7ac05ab5564a
Anjali Chaturvedi <anjaliichaturvedi@gmail.com>
2025-10-11 21:25:21 +0530

    fix: vite app instead of react

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./css/index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const GOOGLE_CLIENT_ID = import.meta.env.REACT_APP_GOOGLE_CLIENT_ID || ""; 
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_APP_GOOGLE_CLIENT_ID || ""; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
